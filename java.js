8db083bb628508299bd61ad51a28bd927276d8d1
Matt Dillon <mddillon47@yahoo.com>
2016-04-05 17:33:32 -0700

    Added files via upload

---
+++ java.js
@@ -0,0 +1,52 @@
+$('document').ready(function(){
+	
+
+	var boxSize = 16;
+	var size = 480 / boxSize; 
+	
+	for (var i=0; i<boxSize; i++){
+		for (var j=0; j<boxSize; j++) {
+			$("#container").append("<div class='squares'></div>");
+			
+		}
+	}
+		$(".squares").css("height", size);
+		$(".squares").css("width", size);
+
+  
+  $('.squares').mouseenter(function(){
+  	$(this).addClass('color');
+  })  
+
+  $('#clear').on('click', function(){
+  	$('.squares').removeClass('color');
+  })
+  
+
+$('#new').on('click', function(){
+	
+var boxSize = prompt("Enter grid amount.");
+$('#container').empty();
+	var size = 480 / boxSize; 
+	for (var i=0; i<boxSize; i++){
+		for (var j=0; j<boxSize; j++) {
+			$("#container").append("<div class='squares'></div>");
+			
+		}
+	}
+	$(".squares").css("height", size);
+	$(".squares").css("width", size);
+
+  
+  $('.squares').mouseenter(function(){
+  	$(this).addClass('color');
+  })  
+
+  $('#clear').on('click', function(){
+  	$('.squares').removeClass('color');
+  	})
+  })
+});
+
+
+
